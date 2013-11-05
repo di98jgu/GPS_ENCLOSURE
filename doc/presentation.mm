cdafb3bd99d8f7a24f73c18a6965e01785da7564
di98jgu <di98jgu@gmail.com>
2013-11-05 12:36:23 +0100

    Rapport and presentation

---
+++ doc/presentation.mm
@@ -0,0 +1,124 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Presentation" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1383311065749"><hook NAME="MapStyle" zoom="2.852">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7"/>
+<font SIZE="16"/>
+<node TEXT="Why should they care ?" POSITION="right" ID="ID_1225283299" CREATED="1383311406019" MODIFIED="1383311423001">
+<edge COLOR="#ff0000"/>
+<node TEXT="Security" ID="ID_1875183117" CREATED="1383486584749" MODIFIED="1383486654155"/>
+<node TEXT="Many uses" ID="ID_266497433" CREATED="1383486660830" MODIFIED="1383486683086"/>
+</node>
+<node TEXT="Intresting slides" POSITION="left" ID="ID_1410922795" CREATED="1383311882003" MODIFIED="1383311892523">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="What is, what could be" POSITION="right" ID="ID_115468042" CREATED="1383319070861" MODIFIED="1383319181585">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="The start" POSITION="left" ID="ID_477024064" CREATED="1383319370556" MODIFIED="1383319378913">
+<edge COLOR="#ff00ff"/>
+<node TEXT="A story" ID="ID_106839128" CREATED="1383486693310" MODIFIED="1383486701558">
+<node TEXT="Take the phone home" ID="ID_660796758" CREATED="1383489110190" MODIFIED="1383492222881"/>
+<node TEXT="Drop in in local store" ID="ID_94306968" CREATED="1383492299786" MODIFIED="1383503970908"/>
+<node TEXT="What was on it ?" ID="ID_947271942" CREATED="1383503939352" MODIFIED="1383503953250"/>
+</node>
+<node TEXT="It need protection, but how ?" ID="ID_1689844890" CREATED="1383504421635" MODIFIED="1383504453833"/>
+</node>
+<node TEXT="The midle" POSITION="right" ID="ID_333186520" CREATED="1383319382028" MODIFIED="1383319387169">
+<edge COLOR="#00ffff"/>
+<node TEXT="We need" ID="ID_1497218824" CREATED="1383504473007" MODIFIED="1383520934453">
+<node TEXT="A fence" ID="ID_1389169710" CREATED="1383504509259" MODIFIED="1383520972896"/>
+<node TEXT="Inform user" ID="ID_964409977" CREATED="1383520979016" MODIFIED="1383520992885"/>
+<node TEXT="The story - Mobil, U have left the area!" ID="ID_51636883" CREATED="1383521017681" MODIFIED="1383521108461"/>
+</node>
+<node TEXT="The limits" ID="ID_1811389460" CREATED="1383521131098" MODIFIED="1383521139168">
+<node TEXT="The mobil unit" ID="ID_910480951" CREATED="1383504565803" MODIFIED="1383521125441">
+<node TEXT="Battery" ID="ID_877314846" CREATED="1383504593827" MODIFIED="1383504603950"/>
+<node TEXT="GPS and other mehtods" ID="ID_1456087670" CREATED="1383504618379" MODIFIED="1383504632405"/>
+</node>
+<node TEXT="The fence" ID="ID_1741989671" CREATED="1383521192042" MODIFIED="1383521195901">
+<node TEXT="Coordinates" ID="ID_1457208362" CREATED="1383521198713" MODIFIED="1383521239466"/>
+</node>
+</node>
+<node TEXT="Solution" ID="ID_296322692" CREATED="1383521284493" MODIFIED="1383521291619">
+<node TEXT="The grid" ID="ID_1612346548" CREATED="1383504640494" MODIFIED="1383504654642">
+<node TEXT="Fast" ID="ID_285505119" CREATED="1383504657533" MODIFIED="1383504673261"/>
+<node TEXT="Properties" ID="ID_709846396" CREATED="1383504676104" MODIFIED="1383504683387"/>
+<node TEXT="Areas not area" ID="ID_97912782" CREATED="1383504685628" MODIFIED="1383504694653"/>
+</node>
+</node>
+<node TEXT="Problems" ID="ID_62738202" CREATED="1383521309474" MODIFIED="1383521322283">
+<node TEXT="Speed of mobil unit" ID="ID_904830928" CREATED="1383521324928" MODIFIED="1383521349197"/>
+<node TEXT="Not able to handle small area" ID="ID_99615391" CREATED="1383521354491" MODIFIED="1383521404525"/>
+</node>
+<node TEXT="A possible fix" ID="ID_228444480" CREATED="1383521410606" MODIFIED="1383521429439">
+<node TEXT="Distance to fence" ID="ID_1048977740" CREATED="1383521432399" MODIFIED="1383521480355"/>
+<node TEXT="By triangle insted" ID="ID_357415456" CREATED="1383521505496" MODIFIED="1383521650621"/>
+</node>
+<node TEXT="Where are we" ID="ID_95909358" CREATED="1383521573364" MODIFIED="1383521623933"/>
+</node>
+<node TEXT="The end" POSITION="left" ID="ID_988572594" CREATED="1383319390059" MODIFIED="1383319393054">
+<edge COLOR="#ffff00"/>
+<node TEXT="Where are we" ID="ID_406813053" CREATED="1383521672799" MODIFIED="1383521677352">
+<node TEXT="Grid dont work" ID="ID_1384920950" CREATED="1383521679800" MODIFIED="1383521687371"/>
+<node TEXT="Need to be able to handle a small area" ID="ID_1325334306" CREATED="1383521721097" MODIFIED="1383521758559"/>
+</node>
+<node TEXT="What I would like to do" ID="ID_1575222125" CREATED="1383521702194" MODIFIED="1383521712731">
+<node TEXT="The bahaviour of the device" ID="ID_430958988" CREATED="1383486812311" MODIFIED="1383486836524"/>
+</node>
+</node>
+<node TEXT="Bilder" POSITION="right" ID="ID_1016593267" CREATED="1383492966781" MODIFIED="1383492970293">
+<edge COLOR="#7c0000"/>
+<node TEXT="BTH omr&#xe5;det" ID="ID_528379635" CREATED="1383492973420" MODIFIED="1383492980474"/>
+<node TEXT="Inh&#xe4;gnad - rutn&#xe4;t - cell" ID="ID_1298982454" CREATED="1383492983895" MODIFIED="1383493000724"/>
+<node TEXT="Droping phone in store" ID="ID_184189624" CREATED="1383493010042" MODIFIED="1383493084689"/>
+</node>
+</node>
+</map>
